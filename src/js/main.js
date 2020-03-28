48e5e13a78f75317f9f6efed5f12365be880034e
daladn0 <markkostevychx@gmail.com>
2020-03-28 23:25:44 +0300

    full adaptive ( 320+ ) & button 'scroll to top'

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,26 @@
-let burger = document.querySelector('.burger');
-let sidebar = document.querySelector('.sidebar');
+$(document).ready(function(){
 
-burger.onclick = function() {
-	burger.classList.toggle('burger_active');
-	sidebar.classList.toggle('sidebar_active');
-};
+	// Smooth scroll & pageup
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1200) {
+      $('.scrolltotop').fadeIn();
+    } else {
+      $('.scrolltotop').fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(_href).offset().top + "px"
+    }, 600);
+    return false;
+  });
+
+  // Sidebar
+	$('.burger').on('click', function(){
+		$(this).toggleClass('burger_active');
+		$('.sidebar').toggleClass('sidebar_active');
+	});
+
+});
